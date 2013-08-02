a1a9b9517ca430e4051d9f9109bd74c808726cb8
Seivan Heidari <seivan@kth.se>
2013-08-02 14:19:07 +0200

    Setup tests.

---
+++ Example/ExampleTests/ExampleTests.m
@@ -0,0 +1,24 @@
+//
+//  ExampleTests.m
+//  ExampleTests
+//
+//  Created by Seivan Heidari on 8/2/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import "ExampleTests.h"
+#import "SHKeyValueObserverBlocks.h"
+
+@implementation ExampleTests
+
+-(void)setUp; {
+  [super setUp];
+}
+-(void)tearDown; {
+  [super tearDown];
+}
+
+-(void)testExample; {
+}
+
+@end
